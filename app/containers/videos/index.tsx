0d3c821d101165aefe9a3ed7c7bc66354c8649eb
David Peter <peterdavid.private@gmail.com>
2023-04-17 17:05:26 +0200

    feat: horizontal scrollable list of videos

--- app/containers/videos/index.tsx
+++ app/containers/videos/index.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import {
   Dimensions,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
   ScrollView,
   StyleSheet,
   View,
@@ -15,13 +17,25 @@ import { useStores } from "../../stores";
 const Videos = () => {
   const { VideosStore } = useStores();
   const { width } = useWindowDimensions(); // Get the width of the screen to use for the ScrollView
+  const [focusedVideoIndex, setFocusedVideoIndex] = React.useState(0);
 
   // Fetch the videos from the API
   useEffect(() => {
     VideosStore.fetch();
   }, [VideosStore]);
 
-  // TODO : Stop the first video when the user scrolls to the next video and start the next video
+  // Handle the scroll event of the ScrollView to get the focused video index
+  const handleScroll = React.useCallback(
+    ({
+      nativeEvent: {
+        contentOffset: { x },
+      },
+    }: NativeSyntheticEvent<NativeScrollEvent>) => {
+      const offset = Math.round(x / width); // Get the offset of the ScrollView
+      setFocusedVideoIndex(offset);
+    },
+    [setFocusedVideoIndex]
+  );
 
   return (
     <View style={AppStyle.container}>
@@ -30,13 +44,16 @@ const Videos = () => {
         style={Styles.scrollView}
         snapToInterval={width} // Snap to the next video
         decelerationRate={"fast"} // Faster decelerationRate to make the user experience better
+        onScroll={handleScroll}
+        scrollEventThrottle={16}
+        pagingEnabled
       >
         {VideosStore.list.map((video, index) => (
           <Video
             key={index}
-            style={(Styles.video, { width: width })} // Set the width of the video to the width of the screen
+            style={[Styles.video, { width }]}
             source={{ uri: video.videoUrl }}
-            shouldPlay={index === 0}
+            shouldPlay={focusedVideoIndex === index}
             useNativeControls={false}
             isLooping
             resizeMode={ResizeMode.COVER}
